25d4c571a32111b34264e34c7c3d31a2e7686529
Corser <mcorser@deloitte.com>
2022-09-02 10:21:58 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ag-grid-sandbox';
+  title = 'Movie Rating Grid';
+  
+  public movieColumnDefs: ColDef[] = [
+	{field: 'title'},
+	{field: 'genre'},
+	{field: 'release_year'},
+	{field: 'numeric_rating'}
+  ];
+  
+  //props are dynamic, this is how it initially loads
+  public defaultColumnAttributes: ColDef = {
+	sortable: true,
+	filter: true
+  };
+  
+  //default data that's displayed in grid.
+  //note that this allows ANY datatype; bad design
+  public rowData$!: Observable<any[]>;
+  
+  //this is used to access the Grid API
+  @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+  
+  constructor(private http: HttpClient) {}
+  
+  /*
+	local call for demo only
+	TODO: Implement Java web service to get
+	prod data
+	TODO: Error handling for this call.
+  */
+  onGridReady(params: GridReadyEvent) {
+	this.rowData$ = this.http.get<any[]>('assets/json/ag-grid-sample-data.json');
+  }
+  
+  //example of a custom button function we can use
+  clearSelection(): void {
+	this.agGrid.api.deselectAll();
+  }
 }
