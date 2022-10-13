02d5998a488d648892664ca95be6b6e573106aa0
Corser <mcorser@deloitte.com>
2022-10-13 13:04:19 -0400

    Note functionality properly saving, but only to localStorage since I have no access to our DB. Next step would be to save that to the database and incorporate this functionality into our actual codebase once access is granted.

--- src/app/note-dialog/note-dialog.component.ts
+++ src/app/note-dialog/note-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { QUILL_CONFIG_TOKEN, QuillConfig } from 'ngx-quill';
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
 import { Study } from '../study';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill'
 
 @Component({
 	selector: "note-dialog",
@@ -10,13 +11,34 @@ import { Study } from '../study';
 })
 
 export class NoteDialogComponent {
+	public noteHtml: any;
 	constructor(public dialogRef: MatDialogRef<NoteDialogComponent>, 
 		@Inject(MAT_DIALOG_DATA) public data: Study,
 		@Inject(QUILL_CONFIG_TOKEN) config: QuillConfig) {
-			console.log(data);
+			this.noteHtml = "";
 	}
 	
 	onClose(): void {
 		this.dialogRef.close();
+	}
+
+	saveChangedNote(event: EditorChangeContent | EditorChangeSelection) {
+		this.noteHtml = event.editor.root.innerHTML;
+	}
+
+	saveNote() {
+		window.localStorage.setItem(this.data.study_id, this.noteHtml);
+		this.dialogRef.close();
+	}
+
+	setInitialNote($event: any) {
+		let savedNote = this.getNote();
+		if (savedNote != null) {
+			$event.root.innerHTML = savedNote;
+		}
+	}
+
+	getNote(): any {
+		return window.localStorage.getItem(this.data.study_id);
 	}
 }
